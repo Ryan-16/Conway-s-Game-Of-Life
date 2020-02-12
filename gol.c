78e7b3a759ef396bdefeb9cca0b009a2690c230a
Ryan <rlail1607@gmail.com>
2020-02-12 13:53:34 +0000

    basic file read in

---
+++ gol.c
@@ -0,0 +1,9 @@
+void read_in_file(FILE *infile, struct universe *u)
+{
+    infile = fopen(, "r");
+
+    if (infile == NULL) {
+        printf("Couldn't open the file\n");
+        return 1;
+    }
+}
