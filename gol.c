74aeb1a9b5ebf6658dd553db81be887a82b06cd3
Ryan <rlail1607@gmail.com>
2020-02-16 22:00:46 +0000

    initial file read in

--- gol.c
+++ gol.c
@@ -1,9 +1,27 @@
 void read_in_file(FILE *infile, struct universe *u)
 {
-    infile = fopen(, "r");
+    // work out number of rows and cells
+    // dynamically allocate the memory
+    // populate the matrix
 
-    if (infile == NULL) {
-        printf("Couldn't open the file\n");
-        return 1;
-    }
+    // maximum length of any given row is 512, so set first row length to 512 then realloc it once know length
+    // start with 1 row and realloc every time
+
+    int current_cols;
+    int maximum_cols;
+
+    // start with 2 rows (so 2 char pointers)
+    u.rows = 2;
+    u.matrix = malloc(u.rows * sizeof(char *));
+
+    u.matrix[0] = malloc(maximum_cols * sizeof(char *));
+
+    current_cols = fscanf(infile, "%c", u.matrix[0]);
+    u.cols = current_cols;
+
+    u.matrix[0] = realloc(u.matrix[0], u.cols * sizeof(char *));
+    u.matrix[1] = malloc(u.cols * sizeof(char *));
+    
+    while (EOF != (current_cols = fscanf(infile, "%c", u.matrix[u.rows - 1])))
+
 }
