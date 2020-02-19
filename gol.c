2392fbd4919b6d9deee25ad7f9db4bf426daaa39
Ryan <rlail1607@gmail.com>
2020-02-19 16:06:10 +0000

    switch to coutning from 0

--- gol.c
+++ gol.c
@@ -1,7 +1,7 @@
-//#include<stdio.h>
-//#include<stdlib.h>
-//#include"gol.h"
-//#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include"gol.h"
+#include<string.h>
 
 void read_in_file(FILE *infile, struct universe *u)
 {
@@ -9,20 +9,22 @@ void read_in_file(FILE *infile, struct universe *u)
     int current_cols;
     int maximum_cols = 512;
 
-    // start with initial 1 row (so 1 char pointers)
-    u->rows = 1;
-    u->matrix = malloc(u->rows * sizeof(char *));
+    // start with initial 1 row (so 1 char pointers), but we use index 0
+    u->rows = 0;
+    u->matrix = malloc((u->rows + 1) * sizeof(char *));
 
-    u->matrix[u->rows - 1] = malloc(maximum_cols * sizeof(char *));
-    u->cols = fscanf(infile, "%s", u->matrix[u->rows - 1]);
-    u->matrix[u->rows - 1] = realloc(u->matrix[u->rows - 1], u->cols * sizeof(char *));
+    u->matrix[u->rows] = malloc(maximum_cols * sizeof(char *));
+    fscanf(infile, "%s", u->matrix[u->rows]);
+    u->cols = strlen(u->matrix[u->rows]);
+    u->matrix[u->rows] = realloc(u->matrix[u->rows], u->cols * sizeof(char *));
 
     u->rows ++;
-    u->matrix = realloc(u->matrix, u->rows * sizeof(char *));
-    u->matrix[u->rows - 1] = malloc(u->cols * sizeof(char *));
+    u->matrix = realloc(u->matrix, (u->rows + 1) * sizeof(char *));
+    u->matrix[u->rows] = malloc(u->cols * sizeof(char *));
      
-    while (-1 != (current_cols = fscanf(infile, "%s", u->matrix[u->rows - 1]))) {
-
+    while (-1 != fscanf(infile, "%s", u->matrix[u->rows])) {
+        
+        current_cols = strlen(u->matrix[u->rows]);
         if (current_cols != u->cols) {
             // stderror
         }
@@ -30,15 +32,18 @@ void read_in_file(FILE *infile, struct universe *u)
         else {
 
             u->rows ++;
-            u->matrix = realloc(u->matrix, u->rows * sizeof(char *));
-            u->matrix[u->rows - 1] = malloc(u->cols * sizeof(char *));
+            u->matrix = realloc(u->matrix, (u->rows + 1) * sizeof(char *));
+            u->matrix[u->rows] = malloc(u->cols * sizeof(char *));
         }
     }
 
     // free up EOF memory
-    free(u->matrix[u->rows - 1]);
+    free(u->matrix[u->rows]);
     u->rows --;
-    u->matrix = realloc(u->matrix, u->rows * sizeof(char *));
+    u->matrix = realloc(u->matrix, (u->rows + 1) * sizeof(char *));
+
+    u->cols --;
+
 
 /*    printf("\n\n");
 
@@ -58,7 +63,7 @@ void write_out_file(FILE *outfile, struct universe *u)
 
 int is_alive(struct universe *u, int column, int row)
 {
-    if (0 == strcmp(u->alive, u->matrix[row][column])) {
+    if (0 == strcmp("*", u->matrix[row][column])) {
         return 1;
     }
 
@@ -75,7 +80,7 @@ int will_be_alive(struct universe *u, int column, int row)
         for(int col_modifier = -1; col_modifier <= 1; col_modifier ++) {
             
             if (row + row_modifier < 0 || row + row_modifier > u->rows ||
-                column + col_modifier < 0 || column + col_modifier > u->cols ||) {
+                column + col_modifier < 0 || column + col_modifier > u->cols) {
                 
                 // out of universe
                 continue;
@@ -87,14 +92,14 @@ int will_be_alive(struct universe *u, int column, int row)
                 continue;
             }
 
-            else if (0 == strcmp(u->alive, u->matrix[row + row_modifier][column + col_modifier])) {
+            else if (0 == strcmp("*", u->matrix[row + row_modifier][column + col_modifier])) {
                 
                 alive_neighbours ++;
             }
         }
     }
 
-    if (0 == strcmp(u->alive, u->matrix[row][column])) {
+    if (0 == strcmp("*", u->matrix[row][column])) {
 
         // alive
 
@@ -118,12 +123,14 @@ int will_be_alive(struct universe *u, int column, int row)
 
 }
 
+//int will_be_alive_torus(struct universe *u, int column, int row)
 
-/*
+
+
 int main() {
 
     struct universe v;
     read_in_file(stdin, &v);
     
     return 0;
-}*/
+}
