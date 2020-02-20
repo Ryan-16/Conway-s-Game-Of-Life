483fffd656eabc1eeb97480ad07b77622d68fe62
Ryan <rlail1607@gmail.com>
2020-02-20 21:56:55 +0000

    command line options implemented

--- gameoflife.c
+++ gameoflife.c
@@ -1,10 +1,48 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<stdbool.h>
 #include"gol.h"
 
 int main(int argc, char *argv[]){
+
+    char option;
+    char *input_file_name;
+    char *output_filename;
+    int number_of_generations = 5;
+    bool print_statistics = false;
+    bool torus = false;
     
+    for (int arg = 1; arg <= argc; arg ++) {
+        // switch on 2nd character of arg ascii val
+        
+        option = argv[arg][1];
+
+        switch(option)
+        {
+            case 'i':
+                arg ++;
+                input_file_name = argv[arg];
+
+            case 'o':
+                arg ++;
+                output_file_name = argv[arg];
+            
+            case 'g':
+                arg ++;
+                number_of_generations = atoi(argv[arg]);
+
+            case 's':
+                print_statistics = true;
+
+            case 't':
+                torus = true;
+
+            default:
+                // unrecognized option
+        }
+
+    }
 
     struct universe v; 
     read_in_file(stdin,&v);
