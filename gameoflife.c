277fd0caf97843c2d6c902cf74635238a1f5d314
Ryan <rlail1607@gmail.com>
2020-02-20 17:52:27 +0000

    clean up formatting of gameoflife

--- gameoflife.c
+++ gameoflife.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include"gol.h"
+
 int main(int argc, char *argv[]){
-  struct universe v; 
-  read_in_file(stdin,&v);
-  evolve(&v,will_be_alive);
-  evolve(&v,will_be_alive);
-  evolve(&v,will_be_alive);
-  evolve(&v,will_be_alive);
-  evolve(&v,will_be_alive);
-  write_out_file(stdout,&v);
-return 0;
+    
+
+    struct universe v; 
+    read_in_file(stdin,&v);
+    evolve(&v,will_be_alive);
+    evolve(&v,will_be_alive);
+    evolve(&v,will_be_alive);
+    evolve(&v,will_be_alive);
+    evolve(&v,will_be_alive);
+    write_out_file(stdout,&v);
+    return 0;
 }
